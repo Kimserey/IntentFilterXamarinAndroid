337c2639bd19481df586bde8ba3ec6f400a5e1a5
Kimserey Lam <kimserey.lam@gmail.com>
2016-12-17 10:36:47 +0000

    intent app one opens intent app two

---
+++ IntentAppOne/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace IntentAppOne
+{
+	[Activity(Label = "Activity ONE", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			button.Click += (sender, e) => {
+				var intent = new Intent("com.kimserey.action.FIND_BASKET");
+				intent.SetFlags(ActivityFlags.NewTask);
+				StartActivity(intent);
+			};
+		}
+	}
+}
+
---
+++ IntentAppTwo/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace IntentAppTwo
+{
+	[Activity(Label = "Activity TWO", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.Main);
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+		}
+	}
+
+	[Activity(Label = "Activity TWO"), IntentFilter(new[] { "com.kimserey.action.FIND_BASKET" }, Categories = new[] { Intent.CategoryDefault })]
+	public class CheckBasket : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState); 
+			SetContentView(Resource.Layout.Checkbasket);
+		}
+
+		protected override void OnNewIntent(Intent intent)
+		{
+			base.OnNewIntent(intent);
+		}
+	}
+}
+
